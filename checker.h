a90f9b940592f942c73a21564506c4ca34ce4d1b
RamkumarSWE <115966371+RamkumarSWE@users.noreply.github.com>
2022-12-13 23:21:20 +0530

    file split

---
+++ checker.h
@@ -0,0 +1,8 @@
+
+int batteryIsOk(float temperature, float soc, float chargeRate);
+int checkTemperature(float temperature);
+int checkSOC(float soc);
+int checkChargeRate(float chargeRate);
+void printTemperatureStatus(float temperature);
+void printSOCStatus(float soc);
+void printChargeRateStatus();
--- checker_main.c
+++ checker_main.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
-#include <assert.h>
-
-int batteryIsOk(float temperature, float soc, float chargeRate);
-int checkTemperature(float temperature);
-int checkSOC(float soc);
-int checkChargeRate(float chargeRate);
-void printTemperatureStatus(float temperature);
-void printSOCStatus(float soc);
-void printChargeRateStatus();
+#include "checker.h"
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   return (checkTemperature(temperature) && checkSOC(soc) \
@@ -60,27 +52,4 @@ void printSOCStatus(float soc) {
 
 void printChargeRateStatus() {
   printf("Charge Rate out of range!\n");  
-}
-
-int main() {
-
-  //Battery Check
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0.8));
-  assert(!batteryIsOk(25, -12, 0.7));
-  assert(!batteryIsOk(-110, 80, 0.7));
-
-  //Battery Temperature Check
-  assert(checkTemperature(33.0));
-  assert(!checkTemperature(-52.0));
-  assert(!checkTemperature(150.0));
-  
-  //Battery SOC Check
-  assert(checkSOC(23.0));
-  assert(!checkSOC(-32.0));
-  assert(!checkSOC(350.0));
-
-  //Battery charge rate check
-  assert(checkChargeRate(0.7));
-  assert(!checkChargeRate(122.0));
 }
