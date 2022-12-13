06016a2e3d4525c53130b92d16a7cd6076bdac19
RamkumarSWE <115966371+RamkumarSWE@users.noreply.github.com>
2022-12-13 23:18:16 +0530

    test cases added

--- checker.c
+++ checker.c
@@ -24,7 +24,7 @@ int checkTemperature(float temperature) {
 
 int checkSOC(float soc) {
   if(soc < 20 || soc > 80) {
-    printSOCStatus();
+    printSOCStatus(soc);
     return 0;
   }
   return 1;
@@ -63,6 +63,24 @@ void printChargeRateStatus() {
 }
 
 int main() {
+
+  //Battery Check
   assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(!batteryIsOk(50, 85, 0.8));
+  assert(!batteryIsOk(25, -12, 0.7));
+  assert(!batteryIsOk(-110, 80, 0.7));
+
+  //Battery Temperature Check
+  assert(checkTemperature(33.0));
+  assert(!checkTemperature(-52.0));
+  assert(!checkTemperature(150.0));
+  
+  //Battery SOC Check
+  assert(checkSOC(23.0));
+  assert(!checkSOC(-32.0));
+  assert(!checkSOC(350.0));
+
+  //Battery charge rate check
+  assert(checkChargeRate(0.7));
+  assert(!checkChargeRate(122.0));
 }
