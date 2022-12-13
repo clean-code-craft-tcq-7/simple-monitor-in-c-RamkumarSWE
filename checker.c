11e16fbc40cd05d2b56325e60d46eeac17367ae1
RamkumarSWE <115966371+RamkumarSWE@users.noreply.github.com>
2022-12-13 23:13:55 +0530

    Typo

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@ int checkTemperature(float temperature);
 int checkSOC(float soc);
 int checkChargeRate(float chargeRate);
 void printTemperatureStatus(float temperature);
-void printSOCStatus(float soc)
+void printSOCStatus(float soc);
 void printChargeRateStatus();
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
